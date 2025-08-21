830cb485468c6f9aeaaa4955a50fe5728611201e
ojaswini8572beaift25-pixel <ojaswini8572.beaift25@chitkara.edu.in>
2025-08-21 21:59:36 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,17 @@
+ 
+
+
+a=int(input("enter marks"))
+if (a>=101) or (a<0):
+    print("marks are invalid")  
+elif (90<=a<100):
+    print("grade is A+")
+elif(75<=a<90):
+    print("grade is A")
+elif(60<=a<75):
+    print("grade is B")
+elif(40<=a<60):
+    print("grade is C")
+else:
+    print("you are fail")    
+                                                   
