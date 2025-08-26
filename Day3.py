9cd890b406426de33b47368d4d8a36f6361844c3
ojaswini8572beaift25-pixel <ojaswini8572.beaift25@chitkara.edu.in>
2025-08-26 10:25:17 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,17 @@
+# #  loops in python:
+# for i in range(1,1001):
+#      print("numbers:",i)
+
+# fruits=["cherry","grapes","mango","guavavava"]
+# print(fruits[0])
+# print(fruits)
+# for fruit in fruits:
+#      print("i like:",fruit)
+
+# while condition
+# code block
+
+i=1
+while i<=5:
+    print("Number:",i)
+    i+=1
