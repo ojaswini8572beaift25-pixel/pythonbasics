347bf6b7181799bedc1a627efbf95c10a733560e
ojaswini8572beaift25-pixel <ojaswini8572.beaift25@chitkara.edu.in>
2025-09-01 19:54:45 +0530

    Add files via upload

---
+++ Day5.py
@@ -0,0 +1,7 @@
+print("Day 5")
+print("Today's topic is nested looping")
+
+for i in range(1,4):
+    for j in range(1,4,):
+        print(i*j,end=" ")
+    print() 
---
+++ functions.py
@@ -0,0 +1,33 @@
+# A function is a block of code that performs a specific task, it helps in code reusebility.
+
+# def_function_name(parameters):
+# function body
+# return value
+
+# def greet(name):
+#     return f"hello,{name}!"
+# print(greet("ojaswini"))
+
+# nums=[10,20,30]
+# print(len(nums))
+# print(max(nums))
+# print(min(nums))
+# print(sum(nums))
+
+#Calling Function
+# def square(x):
+#     return x*x
+# print(square(5))
+
+# def add(a,b):
+#     return a+b
+# res=add(10,20)
+# print("the sum is", res)
+ 
+a=int(input("enter first number :"))
+b=int(input("enter second number :"))
+
+def add(a,b):
+     return a+b
+res=(a+b)
+print("the sum is", res)
