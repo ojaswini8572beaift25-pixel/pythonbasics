3d1be618cca3bbe7382d4e7a55dcd749d65b92ac
ojaswini8572beaift25-pixel <ojaswini8572.beaift25@chitkara.edu.in>
2025-09-09 21:54:14 +0530

    Add files via upload

---
+++ comparisonop.py
@@ -0,0 +1,9 @@
+# Write a program to check whether two numbers are equal using comparison operators
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+if a == b:
+    print("The two numbers are equal.")
+else:
+    print("The two numbers are not equal.")
