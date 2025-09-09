ee362d8a51de3b2358f579018166e6ef594f3512
ojaswini8572beaift25-pixel <ojaswini8572.beaift25@chitkara.edu.in>
2025-09-09 21:48:34 +0530

    Add files via upload

---
+++ name&age.py
@@ -0,0 +1,6 @@
+# Write a program that takes your name and age as input and prints: 'My name is X and I am Y
+#  years old.'
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"My name is {name} and I am {age} years old.")
