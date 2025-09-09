f1f2c79d86ce8fee066094ca6e9cdbd938485f41
ojaswini8572beaift25-pixel <ojaswini8572.beaift25@chitkara.edu.in>
2025-09-09 21:46:36 +0530

    Add files via upload

---
+++ tempconver.py
@@ -0,0 +1,5 @@
+# Write a program that converts Celsius to Fahrenheit.
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius}°C is equal to {fahrenheit}°F")
