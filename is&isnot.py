61ab5bebcc930dcc4d507bf794de6e8110868f8f
ojaswini8572beaift25-pixel <ojaswini8572.beaift25@chitkara.edu.in>
2025-09-09 21:17:38 +0530

    Add files via upload

---
+++ is&isnot.py
@@ -0,0 +1,7 @@
+# Write a program to compare two objects using 'is' and 'is not'.
+
+a = input("Enter first value:")
+b = input("Enter second value:")
+print("a is b:", a is b)
+print("a is not b:", a is not b)
+print("a == b:", a == b)
