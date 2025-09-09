71554c410516aa4626eef9f0500c8406952e3d11
ojaswini8572beaift25-pixel <ojaswini8572.beaift25@chitkara.edu.in>
2025-09-09 21:34:30 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,14 @@
+# Write a program to check the type of variables: integer, float, string, list, tuple, dictionary.
+# a=10
+# b=4.3
+# c="hello!"
+# d= [4,6,8]
+# e=(100,200,300,400)
+# f = {"name": "Ojaswini", "age": 18,"grade": "A"}
+
+# print({type(a)})
+# print({type(b)})
+# print({type(c)})
+# print({type(d)})
+# print({type(e)})
+# print({type(f)})
