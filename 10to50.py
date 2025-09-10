5583711b655a77c6969f805987d3c16619c92dad
ojaswini8572beaift25-pixel <ojaswini8572.beaift25@chitkara.edu.in>
2025-09-10 08:48:46 +0530

    Add files via upload

---
+++ 10to50.py
@@ -0,0 +1,8 @@
+# Write a program to check whether a number lies between 10 and 50 using logical operators
+
+num = int(input("Enter a number: "))
+
+if 10 <= num <= 50:
+      print(f"{num} lies between 10 and 50.")
+else:
+    print(f"{num} does not lie between 10 and 50.")
